6cc0b4993df4b695c8a246d2564f14fe978e5c25
silentmx <silentmx@outlook.com>
2023-02-21 15:49:50 +0800

    修改tsconfig配置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,12 @@
-import 'uno.css';
+import * as components from '@silentmx/v-ui/components';
 import Theme from 'vitepress/theme';
+import type { App } from 'vue';
 import { h } from 'vue';
-import * as components from '../../../src/components';
 import DemoBlock from './components/demo-block.vue';
 import './custom.css';
+
+// Unocss
+import 'uno.css';
 
 export default {
   ...Theme,
@@ -12,9 +15,9 @@ export default {
       // 'home-features-after': () => h(teamMember),
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp(app: App) {
     for (let name of Object.keys(components)) {
-      app.component(name, components[name]);
+      app.component(name, (components as any)[name]);
     }
 
     app.component("Demo", DemoBlock);

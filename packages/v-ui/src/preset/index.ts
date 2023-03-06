39d18e13d0bd8c5a079945e54509e624434d98b4
silentmx <silentmx@outlook.com>
2023-03-06 16:50:42 +0800

    添加overlay-container样式shortcuts

--- packages/v-ui/src/preset/index.ts
+++ packages/v-ui/src/preset/index.ts
@@ -2,8 +2,11 @@ import type { Preset } from "unocss";
 import type { Theme } from "unocss/preset-uno";
 import type { PresetConfig } from "./config";
 import { setPrelight } from "./prelights";
+import { rules } from "./rules";
 import { safelist } from "./safe-list";
+import { shortcuts } from "./shortcuts";
 import { theme } from "./theme";
+import { variants } from "./variants";
 
 export function presetVui(config?: PresetConfig): Preset<Theme> {
   return {
@@ -14,6 +17,9 @@ export function presetVui(config?: PresetConfig): Preset<Theme> {
       theme: 0,
       default: 1,
     },
-    preflights: setPrelight(config)
+    preflights: setPrelight(config),
+    shortcuts,
+    rules,
+    variants
   }
 }
--- packages/v-ui/src/preset/shortcuts.ts
+++ packages/v-ui/src/preset/shortcuts.ts
@@ -2,12 +2,10 @@ import type { UserShortcuts } from "unocss";
 import type { Theme } from "unocss/preset-uno";
 
 export const shortcuts: UserShortcuts<Theme> = [
-  // [/^bg-(.*)$/, ([, c]) => {
-  //   console.log(c);
-  //   if (c == "primary") {
-  //     return `bg-${selectedTheme.value.secondary}`
-  //   } else {
-  //     return `bg-${c}`;
-  //   }
-  // }],
+  // static shortcuts
+  {
+    "vui-overlay-container": "empty-hidden pointer-events-none fixed z-1000 top-0 right-0 left-0 bottom-0"
+  },
+  // dynamic shortcuts
+  // [/^btn-(.*)$/, ([, c]) => `bg-${c}-400 text-${c}-100 py-2 px-4 rounded-lg`],
 ];

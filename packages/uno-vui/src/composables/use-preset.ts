d036ae9abedc743d7048ddfad3d64378e30e1631
silentmx <silentmx@outlook.com>
2023-04-27 14:48:30 +0800

    some fix

--- packages/uno-vui/src/composables/use-preset.ts
+++ packages/uno-vui/src/composables/use-preset.ts
@@ -1,17 +1,15 @@
-import { IconConfig, PresetConfig, ThemeConfig } from "@silentmx/preset";
 import { defaultDocument, useCssVar } from "@vueuse/core";
 import { computed } from 'vue';
 
-
-const presetConfig = computed<PresetConfig>(() => {
+const presetConfig = computed<any>(() => {
   const configString = useCssVar("--unovui-config", defaultDocument?.body, { initialValue: "{}" });
   return JSON.parse(configString.value);
 });
 
-export const themeList = computed<ThemeConfig[]>(() => {
-  return presetConfig.value?.themes as ThemeConfig[] || [];
+export const themeList = computed<any[]>(() => {
+  return presetConfig.value?.themes as any[] || [];
 });
 
-export const presetIcons = computed<IconConfig>(() => {
-  return presetConfig.value?.icons as IconConfig || {};
+export const presetIcons = computed<any>(() => {
+  return presetConfig.value?.icons as any || {};
 });
